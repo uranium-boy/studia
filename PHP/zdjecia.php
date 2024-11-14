f9a0023baf2ac048ec8745e9d2bcc9a52b71a784
uranium-boy <janczara.maciej@gmail.com>
2024-11-14 18:09:50 +0100

    PHP lab3 gallery

--- PHP/zdjecia.php
+++ PHP/zdjecia.php
@@ -4,14 +4,15 @@ if (isset($_POST['zapisz']) && $_POST['zapisz'] == 'Zapisz' && !isset($_GET['pic
     if (is_uploaded_file($_FILES['zdjecie']['tmp_name'])) {
         $typ = $_FILES['zdjecie']['type'];
         if ($typ === 'image/jpeg') {
-            $link = $_FILES['zdjecie']['name'];
-            $link = strtolower($link);
+            $filename = $_FILES['zdjecie']['name'];
+            $filename = strtolower($filename);
+            $linkPath = 'zdjecia/' . $filename;
 
-            move_uploaded_file($_FILES['zdjecie']['tmp_name'], './' . $link);
+            move_uploaded_file($_FILES['zdjecie']['tmp_name'], './' . $linkPath);
 
             $random = uniqid('img_');
-            $zdj = $random . '.jpg';
-            copy($link, './' . $zdj);
+            $zdj = 'miniaturki/' . $random . '.jpg';
+            copy($linkPath, './' . $zdj);
 
             list($width, $height) = getimagesize($zdj);
 
@@ -33,17 +34,19 @@ if (isset($_POST['zapisz']) && $_POST['zapisz'] == 'Zapisz' && !isset($_GET['pic
             $nowe = imagecreatetruecolor($newW, $newH);
             $obraz = imagecreatefromjpeg($zdj);
             imagecopyresampled($nowe, $obraz, 0, 0, 0, 0, $newW, $newH, $width, $height);
-            imagejpeg($nowe, './mini-' . $link, 100);
-            echo "nowe=/mini-$link<br>";
+
+            $miniPath = 'miniaturki/mini-' . $filename;
+            imagejpeg($nowe, $miniPath, 100);
+            echo "nowe=/$miniPath<br>";
             imagedestroy($nowe);
             imagedestroy($obraz);
             unlink($zdj);
 
-            $dlugosc = strlen($link);
+            $dlugosc = strlen($filename);
             $dlugosc -= 4;
-            $link = substr($link, 0, $dlugosc);
+            $filename = substr($filename, 0, $dlugosc);
             echo "link=$link <br>";
-            header('location:zdjecia.php?pic='.$link);
+            header('location:zdjecia.php?pic='.$filename);
         } else {
             header('location:zdjecia.html');
         }
@@ -51,8 +54,28 @@ if (isset($_POST['zapisz']) && $_POST['zapisz'] == 'Zapisz' && !isset($_GET['pic
 }
 
 if (isset($_GET['pic']) && !empty($_GET['pic'])) {
-    echo '<a href="' . $_GET['pic'] . '.jpg">Zdjęcie</a><br>';
-    echo '<a href=mini-' . $_GET['pic'] . ".jpg>Miniatura</a><br><br>";
+    echo '<a href="zdjecia/' . $_GET['pic'] . '.jpg">Zdjęcie</a><br>';
+    echo '<a href="miniaturki/mini-' . $_GET['pic'] . '.jpg">Miniatura</a><br><br>';
     echo '<a href="zdjecia.html">Powrót</a>';
+
+    $handle = opendir('miniaturki/');
+
+    if (!$handle) {
+        echo "Otwracie katalogu nie powiodło się :(";
+        exit();
+    }
+
+    echo "<h4>Galeria zdjęć</h4>";
+    $number = 0;
+
+    while ($file = readdir($handle)) {
+        if ($file !== "." && $file !== "..") {
+            $original = "zdjecia/" . str_replace('mini-', '', $file);
+            echo '<a href="' . $original . '"><img src="miniaturki/' . $file . '" style="margin:10px;"></a>';
+            $number++;
+        }
+    }
+    echo "<p>Aktualnie w galerii jest $number zdjęć.</p>";
+    closedir();
 }
 ?>
